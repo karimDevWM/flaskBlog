72f6061be08c5eb8855f2e6350a1030da4669e18
karimDevWM <ka.trabelsi9@gmail.com>
2022-05-11 00:34:48 +0200

    continue email and password reset

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # instantiate a flask application
 app = Flask(__name__) # (__name__) indicate the name of the module
@@ -14,5 +16,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+# for sending mail
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskBlog import routes
